9b03ec1cdeee5ff75e7fbc9e08af8290f38b1696
unknown <rax2003_7@hotmail.com>
2024-04-08 20:18:21 -0500

    ya se peude usar un MCP para detectar las teclas

--- left/code.py
+++ left/code.py
@@ -32,15 +32,17 @@ class MyKeyboard(KMKKeyboard):
 keyboard = MyKeyboard()
 keyboard.coord_mapping =  [
     0,  1,  2,  3,  4,  5,         
-    #30, 31, 32, 33, 34, 35,
+    30, 31, 32, 33, 34, 35,
     6,  7,  8,  9, 10, 11,
-    #36, 37, 38, 39, 40, 41,
+    36, 37, 38, 39, 40, 41,
     12, 13, 14, 15, 16, 17,        
-    #42, 43, 44, 45, 46, 47,
+    42, 43, 44, 45, 46, 47,
     18, 19, 20, 21, 22, 23,
-    #48, 49, 50, 51, 52, 53,
+    48, 49, 50, 51, 52, 53,
     24, 25, 26, 27, 28, 29,
-    #54, 55, 56, 57, 58, 59 
+    54, 55, 56, 57, 58, 59,
+    60, 61, 62, 63, 64, 65,
+    66, 67, 68, 69, 70, 71 
 ]
 
 split = Split(
@@ -52,7 +54,7 @@ split = Split(
     uart_flip = False,
 )
 keyboard.modules.append(split)
-layer0Asignations = [ KC.NO]*60
+layer0Asignations = [ KC.I]*72
 layer0Asignations[0] =  KC.A
 layer0Asignations[1] =  KC.B
 layer0Asignations[2] =  KC.C
@@ -67,4 +69,5 @@ keyboard.keymap = [
 
 if __name__ == '__main__':
     keyboard.go()
+
 
--- right/code.py
+++ right/code.py
@@ -1,17 +1,25 @@
 print("Starting on S3")
 
 import board
-
+import busio
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.scanners.keypad import KeysScanner
 from kmk.modules.split import Split, SplitSide,SplitType
+from adafruit_mcp230xx.mcp23017 import MCP23017
+from kmk.modules.layers import Layers
+import digitalio
+from digitalio import DigitalInOut, Direction, Pull
 
+import kmk.scanners.digitalio
 
 _KEY_CFG = [
     board.IO1, board.IO2,board.IO3, board.IO4
 ]
+
+i2c = busio.I2C(scl=board.IO6, sda=board.IO5, frequency=100000)
+mcp = MCP23017(i2c, address=0x20)
 
 class MyKeyboard(KMKKeyboard):
     def __init__(self):
@@ -26,19 +34,38 @@ class MyKeyboard(KMKKeyboard):
             max_events=64
         )
 
-keyboard = MyKeyboard()
+col_pins = (mcp.get_pin(8), mcp.get_pin(9), mcp.get_pin(10), mcp.get_pin(11), mcp.get_pin(12), mcp.get_pin(13))
+row_pins = (mcp.get_pin(2), mcp.get_pin(3), mcp.get_pin(4), mcp.get_pin(5), mcp.get_pin(6), mcp.get_pin(7))
+
+class MCPKeyboard(KMKKeyboard):
+    def __init__(self, colPins, rowPins, diodeOrientation):
+        # create and register the scanner
+        self.matrix = kmk.scanners.digitalio.MatrixScanner(
+            cols=colPins,
+            rows=rowPins,
+            diode_orientation=diodeOrientation,
+            pull=digitalio.Pull.UP,
+            rollover_cols_every_rows=None, # optional
+        )
+
+
+keyboard = MCPKeyboard(col_pins, row_pins, DiodeOrientation.COL2ROW)
 keyboard.coord_mapping =  [
-    #0,  1,  2,  3,  4,  5,         
-    30, 31, 32, 33, 34, 35,
+    #0,  1,  2,  3,  4,  5, 
     #6,  7,  8,  9, 10, 11,
-    36, 37, 38, 39, 40, 41,
-    #12, 13, 14, 15, 16, 17,        
-    42, 43, 44, 45, 46, 47,
+    #12, 13, 14, 15, 16, 17,
     #18, 19, 20, 21, 22, 23,
-    48, 49, 50, 51, 52, 53,
     #24, 25, 26, 27, 28, 29,
-    54, 55, 56, 57, 58, 59 
+    #30, 31, 32, 33, 34, 35,
+
+    36, 37, 38, 39, 40, 41,
+    42, 43, 44, 45, 46, 47,
+    48, 49, 50, 51, 52, 53,
+    54, 55, 56, 57, 58, 59,
+    60, 61, 62, 63, 64, 65,
+    66, 67, 68, 69, 70, 71 
 ]
+
 
 split = Split(
     split_side=SplitSide.RIGHT,
@@ -48,16 +75,30 @@ split = Split(
     data_pin2 = board.IO43,#TX
     uart_flip = False,
 )
+#layer = Layers
+#keyboard.modules.append(layer)
 keyboard.modules.append(split)
 
+#keyboard.col_pins = col_pins
+#keyboard.row_pins = row_pins
 
+#keyboard.diode_orientation = DiodeOrientation.ROW2COL
+keyboard.debug_enabled = True
+
+
+layer0Asignations = [ KC.NO]*72
+layer0Asignations[0] =  KC.I
+layer0Asignations[1] =  KC.J
+layer0Asignations[2] =  KC.K
+layer0Asignations[3] =  KC.L
+layer0Asignations[4] =  KC.M
+layer0Asignations[5] =  KC.N
+layer0Asignations[6] =  KC.O
+layer0Asignations[7] =  KC.P
+keyboard.keymap = [
+    layer0Asignations
+]
 
 if __name__ == '__main__':
     keyboard.go()
-
-
-
-
-
-
 
